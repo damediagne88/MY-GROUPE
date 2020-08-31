7243d72835553c037cac17b3314b30baf304cff3
El HADJI DAME DIAGNE <damediagnea@gmail.com>
2020-08-31 14:22:02 +0200

    teste unitaire sur la fonction set-active_route

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -11,10 +11,22 @@ class HelpersTest extends TestCase
     /**
      * A basic unit test example.
      *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function page_title()
     {
         $this->assertEquals('About | Laracarte- List of artisans', page_title('About'));
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @test
+     */
+    public function set_active_route()
+    {
+        $this->get(route('pages.home'));
+        $this->assertEquals('active', set_active_route('pages.home'));
+        $this->assertEquals('', set_active_route('pages.about'));
     }
 }
