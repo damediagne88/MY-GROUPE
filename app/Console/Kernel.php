5419f5e80e9c3e3de36b4b3db536ae597946fbf6
El HADJI DAME DIAGNE <damediagnea@gmail.com>
2020-09-03 13:41:26 +0200

    Authentifiaction avec la methode auth

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
        //
     ];
-
+    
     /**
      * Define the application's command schedule.
      *
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/','PageController@home')->name('pages.home');
 Route::get('/about','PageController@about')->name('pages.about');
 Route::get('/contact','ContactsController@create')->name('contacts.create');
 Route::post('/contact','ContactsController@store')->name('contacts.store');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
