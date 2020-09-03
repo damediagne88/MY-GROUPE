369ed4d59927c5fcd98579be389f17691f30d803
El HADJI DAME DIAGNE <damediagnea@gmail.com>
2020-09-03 15:34:21 +0200

    System D authentification login register logout

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -29,8 +29,31 @@
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Register</a></li>
+          
+          @guest
+
+          <li><a href="{{ route('login')}}">Login</a></li>
+          <li><a href="{{ route('register') }}">Register</a></li>
+          @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+
+          @endguest
           </ul>
         </div><!--/.nav-collapse -->
       </div>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/','PageController@home')->name('pages.home');
 Route::get('/about','PageController@about')->name('pages.about');
 Route::get('/contact','ContactsController@create')->name('contacts.create');
 Route::post('/contact','ContactsController@store')->name('contacts.store');
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes AUTH
+|--------------------------------------------------------------------------
+|
+*/
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
